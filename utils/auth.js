6b2ff5530a3442387fe512c5e7fdf30074bbbc41
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-09-21 12:51:15 -0400

    added basic web routes

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,21 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
   if (!req.session.logged_in) {
-    res.redirect('/login');
+    res.redirect('/');
   } else {
     next();
   }
 };
 
-module.exports = withAuth;
+// checks if user is a doctor
+const authDr = (req, res, next) => {
+  if (!req.session.isDoc) {
+    res.redirect('/');
+  }
+  next();
+};
+
+// check for specific patient auth
+
+
+module.exports = {withAuth, authDr};
