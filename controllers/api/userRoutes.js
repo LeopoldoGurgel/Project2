4570f3254c7d7023c87b11b1704ff086f10a1c56
JD2929 <jandunk@creonova.com>
2023-09-25 16:14:11 -0300

    fixed if the patient already exists issue

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -59,12 +59,24 @@ router.post('/login', async (req, res) => {
     res.status(400).json(error)
   }
 });
-
 router.post('/signup', async (req, res) => {
-  console.log(req.body)
+  try {
+    // Check if a patient with the same email already exists
+    const existingPatient = await Patient.findOne({ email: req.body.email });
 
-  const patient = await Patient.create(req.body);
-res.status(200).json({id:patient.id})
-console.log(patient.id);
-})
+    if (existingPatient) {
+      return res.status(400).json({ error: 'Patient already exists' });
+    }
+
+    // If the patient does not exist, create a new one
+    const patient = await Patient.create(req.body);
+    res.status(200).json({ id: patient.id });
+    console.log(patient.id);
+  } catch (error) {
+    // Handle any other errors that may occur
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
 module.exports = router   
