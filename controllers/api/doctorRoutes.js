c49ea5b26417f6428fcd48da78189978548820b0
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-09-25 09:41:03 -0400

    finished 2 dr routes

--- controllers/api/doctorRoutes.js
+++ controllers/api/doctorRoutes.js
@@ -6,36 +6,15 @@ const { Appointment, Patient } = require('./../../models');
 
 // Patient Information (GET) TBD in patient routes
 
-
 // check for authDoc
-
-// get retrieves
-// post adds
-// put updates
-// delete deletes
-
-
 
 // Doctor search (GET): 
 //     search by name, age, app, condition, clinic/zoom, 
 //     [more info links to patient info page]
+router.get('/')
 
 
 // Add appointment (POST)
-const exampleAppointment = {
-  "appointmentDate": "2023-11-15",
-  "appointmentTime": "3pm",
-  "doctor_id": 1,
-  "patient_id": 1,
-  "reason": "backpain",
-  "anamnesis": "Patient complains about persistent backpain started about a year ago. It is primarily located on lombar region and it gets works after long and hard days of work as a carpenter. The patient deescribes it as very intense (8 out of 10. There is no history of recent trauma.",
-  "physical_exam": "Nothing to keep note about vital signs, cardiorrespiratory, digestive or endocrine systems. Lasegue test is positive on the right.",
-  "initial_diagnosis": "lumbar hernia",
-  "tests_needed": "MRI",
-  "prescription": "Ibuprofen 400mg twice a day for 5 days.",
-  "orientation": "10 days of rest from work. Avoid lifting heavy weights.",
-  "next_appointment": "2023-12-15"
-};
 router.post('/appointment', authDoc, async (req, res) => {
   try {
     const appt = await Appointment.create(req.body)
@@ -61,7 +40,19 @@ router.post('/appointment', authDoc, async (req, res) => {
 
 // edit patient info (PUT)
 // auth to check for a specific patient?
-
+router.put('/:id', (req, res) => {
+  Patient.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+}) 
+.then((data) => {
+  res.json(data);
+})
+.catch((err) => {
+  res.status(500).json(err);
+});
 
 
 module.exports = router;
