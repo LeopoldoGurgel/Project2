a302fe35ce55e32eafc3afd1747216ab03de120e
JD2929 <jandunk@creonova.com>
2023-09-25 15:48:09 -0300

    Fixed conflicts with login

--- public/js/userLogin.js
+++ public/js/userLogin.js
@@ -34,7 +34,7 @@ const loginFormHandler = async (event) => {
     }
   };
   
-  // document
-  //   .querySelector('.login-form')
-  //   .addEventListener('submit', loginFormHandler);
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
   
